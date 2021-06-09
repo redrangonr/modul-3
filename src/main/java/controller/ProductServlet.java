9a7dbc821b93463a4bfdaf5aa69539ed0444b68a
Quocanh <phiquocanh717@gmail.com>
2021-06-09 11:42:41 +0700

    adad

--- src/main/java/controller/ProductServlet.java
+++ src/main/java/controller/ProductServlet.java
@@ -8,10 +8,11 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ProductServlet", value = "/ProductServlet")
 public class ProductServlet extends HttpServlet {
-    private IProductService productServiceService = new ProductService();
+    private IProductService productService = new ProductService();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
@@ -56,16 +57,62 @@ public class ProductServlet extends HttpServlet {
         int Quantity = Integer.parseInt(request.getParameter("Quantity"));
         String Color = request.getParameter("color");
         String Category  = request.getParameter("category");
-        float Price =
+        float Price =Float.parseFloat(request.getParameter("Price"));
 
-        ProductList productLsit = new ProductList(id, name,Price,Quantity,Color,Category);
-        boolean isInserted= productServiceService.update(id,prodcutList);
+        ProductList productList  = new ProductList(id, name,Price,Quantity,Color,Category);
+        boolean isInserted= productService.update(id,productList);
         if(!isInserted){
             request.setAttribute("message","Error!");
         }else {
             request.setAttribute("message","Successful");
         }
-        RequestDispatcher dispatcher = request.getRequestDispatcher("Edit.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Edit.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void createNewProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Create.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void createProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+
+        ProductList productList = new ProductList();
+        boolean isInserted = productService.add(productList);
+        if (!isInserted) {
+            request.setAttribute("message","Error!");
+        }else {
+            request.setAttribute("message", "Successful!");
+        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Create.jsp");
+        dispatcher.forward(request, response);
+    }
+    public void deleteProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        ProductList productList = productService.findById(id);
+        if (productList == null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/error-404.jsp");
+            dispatcher.forward(request, response);
+        }
+        productService.remove(id);
+        response.sendRedirect("/product");
+    }
+    private void showEditProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        ProductList productList = productService.findById(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Edit.jsp");
+        if (productList == null) {
+            dispatcher = request.getRequestDispatcher("/error-404.jsp");
+        }
+        request.setAttribute("product", productList);
+        dispatcher.forward(request, response);
+    }
+
+    private void showListProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<ProductList> productLists;
+        productLists = productService.findAll();
+        request.setAttribute("productList", productLists);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/List.jsp");
         dispatcher.forward(request, response);
     }
 }
+
